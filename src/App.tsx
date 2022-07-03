31f4b743487c238667e784558abe646f64a5548a
hamdain <dain9812@naver.com>
2022-07-03 23:13:00 +0900

    feat: react-quill editor  버전 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactQuill />
     </div>
   );
 }
